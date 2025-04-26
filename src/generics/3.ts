8d64656ab0f406ad6235e5d6adf37e6cb05ea9df
Krus <krus.margo@gmail.com>
2025-04-27 00:16:30 +0300

    repair obj

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -9,11 +9,11 @@ function merge<T extends object, P extends object>(objA: T, objB: P): T & P {
 	const objA ={ name: 'Max'}
 	const objB ={age: 20 }
 
-	const mergedObject(objA, objB);
+	const mergedObject=merge(objA, objB);
 
 	console.log(mergedObject); //*{name: 'Max', age: 20}
 
-	
+
 //   У вас є функція merge, яка поєднує два об'єкти. Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 
 
